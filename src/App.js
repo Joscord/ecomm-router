0970ec2c8700215ba35a6fd78a816a8172f7b530
joscord <josecorderodev@gmail.com>
2022-03-01 08:31:02 -0300

    update: deleted icon files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
 
-// pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
